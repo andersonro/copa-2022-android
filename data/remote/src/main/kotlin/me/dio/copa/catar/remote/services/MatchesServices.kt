c1bd80f1ebb75ed5c252e88e952d780adb5f611e
Anderson Roberto de Oliveira <andersonrobertobru@gmail.com>
2023-05-07 16:53:38 -0300

    Update Todo project, update remote endpoint

--- data/remote/src/main/kotlin/me/dio/copa/catar/remote/services/MatchesServices.kt
+++ data/remote/src/main/kotlin/me/dio/copa/catar/remote/services/MatchesServices.kt
@@ -4,6 +4,6 @@ import me.dio.copa.catar.remote.model.MatchRemote
 import retrofit2.http.GET
 
 interface MatchesServices {
-    @GET("api.json")
+    @GET("api-copa-2022.json")
     suspend fun getMatches(): List<MatchRemote>
 }
